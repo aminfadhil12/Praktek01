33332ba93730ce6fb71d58943638f592db5da762
aminfadhil12 <aminfadhil.fadhil268@gmail.com>
2018-04-11 11:58:31 +0800

    Membuat Class Baru

---
+++ src/praktek01/Mahasiswa.java
@@ -0,0 +1,18 @@
+
+package praktek01;
+
+/**
+ *
+ * @author Muhammad Amin Fadhil
+ */
+public class Mahasiswa {
+    String nama;
+    String NPM;
+    double IPK;
+    
+    void CetakInfo () {
+        System.out.println("Nama :"+nama);
+        System.out.println("NPM  :"+NPM);
+        System.out.println("IPK  :"+IPK);
+    }
+}
