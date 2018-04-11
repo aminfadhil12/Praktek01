1e89148fe40c7d0bf857f15c6baa837476143e21
aminfadhil12 <aminfadhil.fadhil268@gmail.com>
2018-04-11 12:00:36 +0800

    Class

---
+++ src/praktek01/Praktek01.java
@@ -0,0 +1,17 @@
+
+package praktek01;
+
+/**
+ *
+ * @author Muhammad Amin Fadhil
+ */
+public class Praktek01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
